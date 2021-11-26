f1d74a15716b238d89a92fa5f29317832062986c
Nathan Innes <nathaninnes001@gmail.com>
2021-11-26 04:39:31 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("Guess the name");
+
+    let names: Vec<&str> = vec!["Erick", "Sabes", "Holland", "Falcon"];
+    let name_value = names[(rand::thread_rng().gen_range(0..names.len()))];
+
+    loop {
+        let mut guess: String = String::new();
+
+        println!("Secret name is: {}", name_value);
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed");
+
+        if check_guess_not_string(&mut guess) {
+            if guess.trim() == name_value {
+                println!("You win");
+                break;
+            } else {
+                println!("Try again");
+                println!("Input got: {}", guess);
+            }
+        } else {
+            println!("Please type a string");
+        }
+    }
+}
+
+fn check_guess_not_string(x: &String) -> bool{
+    return match x.trim().parse::<f64>() {
+        Ok(_) => {
+            false
+        }
+        Err(_) => {
+            true
+        }
+    }
+}
